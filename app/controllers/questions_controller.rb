9c05f4c9794c9a59ceb37779d9d175121192b899
Silvia Neves <siltatiana.sn@gmail.com>
2022-08-22 13:31:08 +0100

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+
+  def answer
+    if params[:question] == 'I am going to work'
+      @answer = 'Great!'
+    elsif params[:question].end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
